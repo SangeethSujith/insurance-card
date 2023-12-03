a4f59a894863848bf8be63120df5a70d665be1bc
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-12-03 19:04:32 +0530

    Added basic stucture and styling

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from '../../components/navbar'
+import Separator from '../../components/Separator'
+import Card from '../../components/Card'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='flex flex-col items-center'>
+            <Navbar />
+            <Card/>
+            <Separator/>
+        </div>
+    )
 }
 
 export default Home
