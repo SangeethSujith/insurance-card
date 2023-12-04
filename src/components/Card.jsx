dd1cff8fd7074a093117a0ff1bf822f9a9ebd1e9
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-12-04 15:29:21 +0530

    Change class into classname

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 const Card = ({heading,desc}) => {
     return (
         <div
-            class="w-[90%] h-auto duration-500 group overflow-hidden relative rounded-xl bg-neutral-800 text-neutral-50 p-4 flex flex-col justify-evenly"
+            className="w-[90%] h-auto duration-500 group overflow-hidden relative rounded-xl bg-neutral-800 text-neutral-50 p-4 flex flex-col justify-evenly"
         >
             <div
-                class="absolute blur duration-500 group-hover:blur-none w-72 h-72 rounded-full group-hover:translate-x-12 group-hover:translate-y-12 bg-sky-900 right-1 -bottom-24"
+                className="absolute blur duration-500 group-hover:blur-none w-72 h-72 rounded-full group-hover:translate-x-12 group-hover:translate-y-12 bg-sky-900 right-1 -bottom-24"
             ></div>
             <div
-                class="absolute blur duration-500 group-hover:blur-none w-12 h-12 rounded-full group-hover:translate-x-12 group-hover:translate-y-2 bg-indigo-700 right-12 bottom-12"
+                className="absolute blur duration-500 group-hover:blur-none w-12 h-12 rounded-full group-hover:translate-x-12 group-hover:translate-y-2 bg-indigo-700 right-12 bottom-12"
             ></div>
             <div
-                class="absolute blur duration-500 group-hover:blur-none w-36 h-36 rounded-full group-hover:translate-x-12 group-hover:-translate-y-12 bg-indigo-800 right-1 -top-12"
+                className="absolute blur duration-500 group-hover:blur-none w-36 h-36 rounded-full group-hover:translate-x-12 group-hover:-translate-y-12 bg-indigo-800 right-1 -top-12"
             ></div>
             <div
-                class="absolute blur duration-500 group-hover:blur-none w-24 h-24 bg-sky-700 rounded-full group-hover:-translate-x-12"
+                className="absolute blur duration-500 group-hover:blur-none w-24 h-24 bg-sky-700 rounded-full group-hover:-translate-x-12"
             ></div>
-            <div class="z-10 flex flex-col justify-evenly w-full h-full">
-                <span class="text-2xl font-bold">{heading}</span>
+            <div className="z-10 flex flex-col justify-evenly w-full h-full">
+                <span className="text-2xl font-bold">{heading}</span>
 
-                <p className='py-2'>
+                <p classNameName='py-2'>
                     {desc}
                 </p>
                 <button
-                    class="hover:bg-neutral-200 bg-neutral-50 rounded text-neutral-800 self-center font-extrabold w-full md:w-[50%] p-3"
+                    className="hover:bg-neutral-200 bg-neutral-50 rounded text-neutral-800 self-center font-extrabold w-full md:w-[50%] p-3"
                 >
                     Know more
                 </button>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -4,13 +4,13 @@ import { variables } from '../constants/variables'
 
 const ProfileCard = () => {
     return (
-        <div class="w-[90%] rounded-lg border-2 border-indigo-500 bg-transparent p-4 mt-4 text-center shadow-lg dark:bg-gray-800">
-            <h2 class="text-xl font-bold text-indigo-600 dark:text-indigo-400">{variables.name}</h2>
-            <p class="mb-4 text-gray-600 dark:text-gray-300">{variables.designation}</p>
-            <div class="flex items-center justify-center">
-                <a href="#" class="rounded-full bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 dark:bg-indigo-400 dark:hover:bg-indigo-500">Contact
+        <div className="w-[90%] rounded-lg border-2 border-indigo-500 bg-transparent p-4 mt-4 text-center shadow-lg dark:bg-gray-800">
+            <h2 className="text-xl font-bold text-indigo-600 dark:text-indigo-400">{variables.name}</h2>
+            <p className="mb-4 text-gray-600 dark:text-gray-300">{variables.designation}</p>
+            <div className="flex items-center justify-center">
+                <a href="#" className="rounded-full bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 dark:bg-indigo-400 dark:hover:bg-indigo-500">Contact
                  </a>
-                <a href="#" class="ml-4 rounded-full bg-gray-300 px-4 py-2 hover:bg-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600">Whatsapp</a>
+                <a href="#" className="ml-4 rounded-full bg-gray-300 px-4 py-2 hover:bg-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600">Whatsapp</a>
             </div>
         </div>
     )
